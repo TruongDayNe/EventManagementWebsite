1f394cedb274ce6dee858429d112ddd95e7a5819
Phan Thanh Truong <lostpeanut123@gmail.com>
2025-05-29 20:30:35 +0700

    feat: change CORS

--- backend/EventManagementWebAPI/Program.cs
+++ backend/EventManagementWebAPI/Program.cs
@@ -57,7 +57,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
     {
-        builder.WithOrigins("http://localhost:5173", "http://localhost", "localhost:80")
+        builder.AllowAnyOrigin()
                .AllowAnyMethod()
                .AllowAnyHeader()
                .AllowCredentials();
